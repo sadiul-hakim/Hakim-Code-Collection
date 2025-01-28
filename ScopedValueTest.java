75570c973a80453d19c594bf7c914aa3de4018f6
Hakim's Garage <92100853+sadiul-hakim@users.noreply.github.com>
2025-01-28 14:19:08 +0600

    Update ScopedValueTest.java

--- ScopedValueTest.java
+++ ScopedValueTest.java
@@ -1,32 +1,43 @@
 public class ScopedValueTest {
-	
+
 	public static final ScopedValue<String> SCOPED_VALUE = ScopedValue.newInstance();
-	
+
 	public static void main(String[] args) {
-		
-		if(SCOPED_VALUE.isBound()) {
-			System.out.println("Beginning of the Main method : "+SCOPED_VALUE.get());
+
+		if (SCOPED_VALUE.isBound()) {
+			System.out.println(Thread.currentThread() + " Beginning of the Main method : " + SCOPED_VALUE.get());
 		}
-		
+
 		// Scoped Value is Immutable.
-		// Value bound to ScopedValue is not only associated with that very Thread also that very scope.
-		// The moment the method completes, The ScopedValue value is unbound and it will be cleared up automatically.
+		// Value bound to ScopedValue is not only associated with that very Thread also
+		// that very scope.
+		// The moment the method completes, The ScopedValue value is unbound and it will
+		// be cleared up automatically.
 		try {
-			ScopedValue.callWhere(SCOPED_VALUE, "New Call", ()->{
+			ScopedValue.callWhere(SCOPED_VALUE, "New Call", () -> {
+
+				Thread.ofPlatform().name("New Thread::").start(() -> {
+					
+					// isBound is false as this code is inside a new thread
+					if (SCOPED_VALUE.isBound()) {
+						System.out.println(Thread.currentThread() + " Beginning of the  Scope under New Thread : " + SCOPED_VALUE.get());
+					}
+				});
 				
-				if(SCOPED_VALUE.isBound()) {
-					System.out.println("Beginning of the  Scope : "+SCOPED_VALUE.get());
+				if (SCOPED_VALUE.isBound()) {
+					System.out.println(Thread.currentThread() + " Beginning of the  Scope : " + SCOPED_VALUE.get());
 				}
-				
+
 				return "";
 			});
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		if(SCOPED_VALUE.isBound()) {
-			System.out.println("End of the Main method : "+SCOPED_VALUE.get());
+
+		// isBound is false as this code is outside of the dynamic scope
+		if (SCOPED_VALUE.isBound()) {
+			System.out.println(Thread.currentThread() + " End of the Main method : " + SCOPED_VALUE.get());
 		}
 	}
 }
